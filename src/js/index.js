b9b7a710bb13100d7863f54c92c7def5f60f09d2
ilPas <p4squy@gmail.com>
2018-10-30 10:40:16 +0100

    Aggiornata settings folder a vari files

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import copenhagen from './copenhagen';
 import TweenMax from './TweenMax.min';
+//import Chatbot from '@hype/chatbot/umd/@hype/chatbot';
 // import gsap from './animation.gsap';
 // import ScrollMagic from './ScrollMagic';
 import scripts from './scripts';
